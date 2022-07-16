1f2a3d4131c5817f784b560b4fd0e31194dfd407
Viktor Titov <goldsphere@gmail.com>
2022-07-17 03:43:41 +1000

    Add crown fly script

---
+++ Assets/Source/CrownFly.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace GMTKGame
+{
+    internal class CrownFly : MonoBehaviour
+    {
+        [SerializeField] private AnimationCurve _flyAnimationCurve;
+        private Transform _transform;
+        private float _time;
+        private Vector3 _position;
+
+        private void Awake()
+        {
+            _transform = GetComponent<Transform>();
+            _position = transform.position;
+        }
+
+        private void Update()
+        {
+            var newPosition = new Vector3(_position.x,
+                _position.y + _flyAnimationCurve.Evaluate(_time), _position.z);
+            _transform.position = newPosition;
+            _time += Time.deltaTime;
+        }
+    }
+
+}
