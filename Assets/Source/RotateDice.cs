c8cc6ea588caa355224226a6e4fa9f9ef83bc6a4
Viktor Titov <goldsphere@gmail.com>
2022-07-17 01:10:02 +1000

    Add rotation to dice in main menu

---
+++ Assets/Source/RotateDice.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GMTKGame
+{
+    class RotateDice : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _rotationSpeed;
+        private Transform _transform;
+
+        private void Awake()
+        {
+            _transform = GetComponent<Transform>();
+        }
+
+        private void Update()
+        {
+            _transform.rotation = Quaternion.Euler(_rotationSpeed) * _transform.rotation;
+        }
+    }
+}
