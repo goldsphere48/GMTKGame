7e4f5fe89000ab8657611916fe3d2cfeb71d03bd
Viktor Titov <goldsphere@gmail.com>
2022-07-17 01:20:01 +1000

    Add Follow player script

---
+++ Assets/Source/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace GMTKGame
+{
+    internal class FollowPlayer : MonoBehaviour
+    {
+        [SerializeField] private GameObject _player;
+
+        private Transform _transform;
+        private Vector3 _distanceToPlayer;
+
+        private void Awake()
+        {
+            _transform = transform;
+            _distanceToPlayer = _transform.position - _player.transform.position;
+        }
+
+        private void Update()
+        {
+            _transform.position = _distanceToPlayer + _player.transform.position;
+        }
+    }
+}
