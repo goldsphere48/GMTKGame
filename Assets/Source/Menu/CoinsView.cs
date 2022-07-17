3f2b2f61a10edb43c36ba735767a5034606765d4
Viktor Titov <goldsphere@gmail.com>
2022-07-18 01:21:39 +1000

    Add coins collected view to main menu

---
+++ Assets/Source/Menu/CoinsView.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace GMTKGame.Menu
+{
+    internal class CoinsView : MonoBehaviour
+    {
+        [SerializeField] private WorldData _worldData;
+        [SerializeField] private TextMeshProUGUI _coinsCountText;
+
+        private void Awake()
+        {
+            _coinsCountText.text = $"{_worldData.CoinsCollected} / {_worldData.CoinsTotal}";
+        }
+    }
+}
